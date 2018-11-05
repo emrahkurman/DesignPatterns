9c428990f2dee0c0b0a91aac923e5bb88cf4cc06
Emrah Kurman <emrahkurman1805@gmail.com>
2018-11-05 13:43:13 +0300

    DesignPatterns with C#

---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager=new CustomerManager(new LoggerFactory2());
+            customerManager.Save();
+
+            Console.ReadLine();
+        }
+    }
+
+    public class LoggerFactory:ILoggerFactory
+    {
+        public ILogger CreateLogger()
+        {
+            //Business to decide factory
+            return new EdLogger();
+        }
+    }
+
+    public class LoggerFactory2 : ILoggerFactory
+    {
+        public ILogger CreateLogger()
+        {
+            //Business to decide factory
+            return new Log4NetLogger();
+        }
+    }
+
+    public interface ILoggerFactory
+    {
+        ILogger CreateLogger();
+    }
+
+    public interface ILogger
+    {
+        void Log();
+    }
+
+    public class EdLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with EdLogger");
+        }
+    }
+
+    public class Log4NetLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with Log4NetLogger");
+        }
+    }
+
+    public class CustomerManager
+    {
+        private ILoggerFactory _loggerFactory;
+        private ILogger _logger;
+        public CustomerManager(ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+            _logger = _loggerFactory.CreateLogger();
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Saved!");
+           
+            _logger.Log();
+        }
+    }
+}
